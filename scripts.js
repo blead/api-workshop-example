6dd5e29c728af587b5cd6974e5ad9b1077837726
blead <blead010@gmail.com>
2016-12-07 00:40:10 +0700

    implement simple request function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+function request(method,url,args) {
+  $.ajax({
+    method: method,
+    url: url,
+    data: args
+  }).done( (response) => {
+    console.log(response);
+  });
+}
+
+request('GET','http://httpbin.org/get',{a:1,b:'two',c:'3'});
